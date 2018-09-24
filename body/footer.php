309c3530c8091e391b4c741de241ca6faade2f7e
Pierre <pierre.fabien.amadeusmobile@gmail.com>
2018-09-24 10:46:55 +0200

    First Commit

---
+++ body/footer.php
@@ -0,0 +1,27 @@
+<div class="col-12 row footer d-flex flex-row align-items-center my-flex-container ">
+  <div class="col-12 col-lg-3 row d-flex flex-row align-items-center my-flex-container head-logo">
+    <a class="col-2 col-sm-2 col-md-2  col-lg-4 d-flex flex-row align-items-center my-flex-container  mx-auto" href="Home.php">
+    <img class="rounded mx-auto d-block img-fluid" src="asset/img/logo.png" alt="">
+    </a>
+  </div>
+  <div class="col-12 col-lg-9 footer-link">
+  <a href="Faq.php" class="col-4 col-lg-3 footer-menu-item">
+    <li>
+      <div class="col-12 footer-menu-line faq"></div>
+      <h6>FAQ <span class="footer-menu-line-span">= foire aux questions</span></h6>
+    </li>
+  </a>
+  <a href="Blog.php" class="col-4 col-lg-3 footer-menu-item">
+    <li>
+      <div class="col-12 footer-menu-line actualite"></div>
+      <h6>Actualité <span class="footer-menu-line-span">Toutes les infos sur la route</span></h6>
+    </li>
+  </a>
+  <a href="Contact.php" class="col-4 col-lg-3 footer-menu-item">
+    <li>
+      <div class="col-12 footer-menu-line contact"></div>
+      <h6>Contact <span class="footer-menu-line-span">toutes les coordonnées</span></h6>
+    </li>
+  </a>
+  </div>
+</div>
---
+++ body/head.php
@@ -0,0 +1,96 @@
+<div class="row head">
+  <!-- <div class="col-12 container-fluid head-first">
+    Site non affilié à la source officielle de l'ANTS. </span>Service privé habilité et agréé par le Ministère de l'Intérieur. Possibilité d'utiliser le site de l'<a href="https://ants.gouv.fr" target="_blank">ANTS</a> sans frais.
+  </div> -->
+  <div class="col-12 row head-img">
+    <div class="col-12 col-lg-3 row d-flex align-items-center head-logo">
+      <a class="col-8 col-sm-6 mx-auto" href="Home.php">
+      <img class="rounded mx-auto d-block img-fluid" src="asset/img/logo.png" alt="">
+      </a>
+      <div class="col-10 mx-auto head-ref row d-flex flex-row align-items-center my-flex-container">
+        <div class="col-10 mx-auto head-ref--box">
+          <p>Habilitation Ministère de l’Intérieur : <strong>n°222442</strong> </p>
+          <p>Agrément Trésor Public : <strong>n°55337</strong></p>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-12 col-lg-3 float-left head-login d-flex flex-row align-items-center my-flex-container">
+      <div class="col-10 mx-auto">
+        <form class="form-horizontal" role="form" method="POST" action="/login">
+          <div class="row">
+              <h2>Connectez-vous</h2>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="form-group has-danger">
+                <label class="sr-only" for="email">E-Mail Address</label>
+                <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                  <div class="input-group-addon" style="width: 1rem"><i class="fa fa-at"></i></div>
+                  <input type="text" name="email" class="form-control" id="email" placeholder="you@example.com" required autofocus>
+                </div>
+              </div>
+            </div>
+            <!-- <div class="col-md-12">
+              <div class="form-control-feedback">
+                <span class="text-danger align-middle">
+                             <i class="fa fa-close"></i> Message d'erreur (exemple)
+                         </span>
+              </div>
+            </div> -->
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label class="sr-only" for="password">Mot de passe</label>
+                <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                  <div class="input-group-addon" style="width: 1rem"><i class="fa fa-key"></i></div>
+                  <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+                </div>
+              </div>
+            </div>
+            <!-- <div class="col-md-3">
+              <div class="form-control-feedback">
+                <span class="text-danger align-middle">
+                        Put password error message here
+                         </span>
+              </div>
+            </div> -->
+          </div>
+          <div class="row">
+            <div class="col-md-12" style="padding-top: .35rem">
+              <div class="form-check mb-2 mr-sm-2 mb-sm-0">
+                <label class="form-check-label">
+                             <input class="form-check-input" name="remember"
+                                    type="checkbox" >
+                             <span style="padding-bottom: .15rem">Se rappeler de moi</span>
+                         </label>
+              </div>
+              <a class="btn btn-link" style="padding:  0px !important;" href="/password/reset">Oublie de votre mot de passe ?</a>
+            </div>
+          </div>
+          <div class="row" style="padding-top: 0.5rem">
+            <div class="col-md-12">
+              <button type="submit" class="col-12 btn btn-success"><i class="fa fa-sign-in"></i> Valider</button>
+
+            </div>
+          </div>
+        </form>
+      </div>
+
+    </div>
+    <!-- <div class="col-5 col-md-3 mx-auto row d-flex flex-row align-items-center my-flex-container">
+      <div class="col-10 mx-auto iconhead row d-flex flex-row align-items-center my-flex-container">
+        <p class="lead row d-flex flex-row align-items-center my-flex-container">
+          <span class="question col-12"> Une question ?</span><br>
+
+          <span class="col-12 col-md-8 mx-auto "><img src="//cdn.demarchescartegrise.com/img/telephone-bas.png" class="rounded mx-auto d-block " alt="telephone"></span>
+          <span class="legal pull-right">Du lundi au vendredi de 9h à 19h</span>
+
+
+        </p>
+      </div>
+    </div> -->
+  </div>
+</div>
